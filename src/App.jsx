fbf356855ab57db644f61f6d675ce41100179053
Raj Kumar <coding.godzilla@gmail.com>
2023-05-05 08:44:30 +0530

    SSO button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 
 function App() {
+  const loginHandler = () => {
+    console.log("I was clicked !!");
+  };
   return (
     <>
       <div className="m-[20px]">
         <p className="text-3xl font-bold underline">Hello world !</p>
         <p className="hand-made-red">I am hand-made</p>
+        <button onClick={loginHandler}>Login with SSO</button>
       </div>
     </>
   );
