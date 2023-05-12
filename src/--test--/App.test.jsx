c875b0d8dcd722db43e5631ccf6df532c2bcd642
deepthi tirupati <deepthi.tirupathi@backcountry.com>
2023-05-12 17:33:52 +0530

    deleted files

--- src/--test--/App.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {render} from '@testing-library/react';
-import App from '../App';
-import {BrowserRouter} from 'react-router-dom'
-
-test('render App page',()=>{
-    render(<App />, {wrapper: BrowserRouter})
-})
