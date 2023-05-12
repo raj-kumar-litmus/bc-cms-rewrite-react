f65f34963fe7325bd86adbdb2c0539d3c0ff3682
deepthi tirupati <deepthi.tirupathi@backcountry.com>
2023-05-12 17:37:44 +0530

    uncomment the logo

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ function App() {
       <div className="flex items-center justify-center mx-auto md:h-screen">
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="sm:p-8">
-          {/* <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center">
             <img src={BackcountryLogo} alt="Backcountry SVG"/>
-          </div> */}
+          </div>
                 <div className="m-[40px]">
                     <div className="relative z-0 px-2 w-full">
                       <button type="submit" onClick={()=> navigate('/sso')} className="w-full text-white bg-black hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 rounded font-medium text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Log in with SSO</button>
