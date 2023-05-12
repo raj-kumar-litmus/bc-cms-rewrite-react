fab1091bb4c18a06ceabb1fdd9ae5106d6ffdb40
deepthi tirupati <deepthi.tirupathi@backcountry.com>
2023-05-12 11:39:03 +0530

    removed unessary class in menu chooser

--- src/components/menuChooser.jsx
+++ src/components/menuChooser.jsx
@@ -1,100 +1,82 @@
-import { useState, useEffect, useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import configurations from "../config";
-import NormalizationImg from "../logos/Normalization-img.svg";
-import SizingAppImg from "../logos/SizingApp-img.svg";
-import ChoosingAppArrow from "../logos/ChoosingApp-arrow.svg";
+import { useNavigate } from 'react-router-dom';
+import NormalizationImg from '../logos/Normalization-img.svg';
+import SizingAppImg from '../logos/SizingApp-img.svg';
+import ChoosingAppArrow from '../logos/ChoosingApp-arrow.svg'
 
 function MenuChooser() {
   const navigate = useNavigate();
-  const [memberGroups, setGroups] = useState([
-    "Data Normalization app",
-    "Sizing app"
-  ]);
+  const { search } = useLocation();
+  const [memberGroups, setGroups] = useState(['Data Normalization app','Sizing app']);
   const { azureUserGroups } = configurations[process.env.NODE_ENV];
-  const [groups] = useSessionStorage("userGroups");
 
   useEffect(() => {
-    if (azureUserGroups) {
-      console.log("groups");
-      console.log(groups);
+    if (azureUserGroups && search) {
+      const groups = new URLSearchParams(search).get("groups").split(",");
       const groupsToAccess = groups
-        ?.filter((groupId) => !!azureUserGroups[groupId])
+        .filter((groupId) => !!azureUserGroups[groupId])
         .map((id) => azureUserGroups[id]);
       if (groupsToAccess) {
         // setGroups(groupsToAccess);
       }
     }
-  }, [azureUserGroups]);
+  }, [azureUserGroups, search]);
+
 
   return (
     <div className="text-center h-screen bg-bg">
-      <h1 className="text-xs font-bold leading-tight tracking-tight text-gray-900 md:text-xs dark:text-white pt-10">
-        Choose the Application
-      </h1>
-      <p className="text-sm mt-4">
-        You Have Permission to access Data Normalization and <br />
-        Sizing app. You can select one from below
-      </p>
+      <h1 className="text-xs font-bold text-black md:text-xs pt-10">Choose the Application</h1>
+      <p className="text-sm mt-4">You Have Permission to access Data Normalization and <br/>Sizing app. You can select one from below</p>
       <div className="flex flex-row justify-center items-center">
-        {memberGroups.filter((item) => item == "Data Normalization app")
-          .length == 1 && (
-          <div className="bg-white rounded-lg shadow dark:border md:m-10">
-            <button onClick={() => navigate("/normalizationDashBoard")}>
-              <div className="p-6 space-y-4 md:space-y-6 sm:p-4">
-                <img src={NormalizationImg} alt="SizingAppImg SVG" />
-              </div>
-              <div>
-                <h1 className="text-xs font-bold leading-tight tracking-tight text-gray-900 md:text-xs dark:text-white">
-                  Data Normalization App
-                </h1>
-                <p className="text-sm m-6">
-                  Lorem ipsum dolor sit amet, <br />
-                  consectetur adipiscing
-                </p>
-              </div>
-
-              <div className="bg-sky">
-                <img
-                  className="bg-amber-300 ml-auto p-1"
-                  src={ChoosingAppArrow}
-                  alt="ChoosingAppArrow SVG"
-                />
-              </div>
-            </button>
+      
+      {memberGroups.filter((item)=> item == "Data Normalization app").length == 1 && <div className="bg-white rounded-lg shadow dark:border md:m-10">
+      <button onClick={()=> navigate('/normalizationDashBoard')}>
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-4">
+              <img
+                 src={NormalizationImg}
+                 alt="SizingAppImg SVG" />
           </div>
-        )}
-
-        {memberGroups.filter((item) => item == "Sizing app").length == 1 && (
-          <div className="bg-white rounded-lg shadow dark:border md:m-10">
-            <button onClick={() => navigate("/sizing-app-dashBoard")}>
-              <div className="p-6 space-y-4 md:space-y-6 sm:p-4">
-                <img src={SizingAppImg} alt="SizingAppImg SVG" />
-              </div>
-
-              <div>
-                <h1 className="text-xs font-bold leading-tight tracking-tight text-gray-900 md:text-xs dark:text-white">
-                  Sizing App
-                </h1>
-                <p className="text-sm m-6">
-                  Lorem ipsum dolor sit amet, <br />
-                  consectetur adipiscing
-                </p>
-              </div>
-
-              <div className="bg-sky">
-                <img
-                  className="bg-amber-300 ml-auto p-1"
-                  src={ChoosingAppArrow}
-                  alt="ChoosingAppArrow SVG"
-                />
-              </div>
-            </button>
+          <div>
+          <h1 className="text-xs font-bold leading-tight tracking-tight text-gray-900 md:text-xs dark:text-white">Data Normalization App</h1>
+          <p className="text-sm m-6">Lorem ipsum dolor sit amet, <br/>consectetur adipiscing</p>
           </div>
-        )}
-        <div></div>
+
+          <div className="bg-sky">
+          <img
+          className="bg-amber-300 ml-auto p-1"
+              src={ChoosingAppArrow}
+              alt="ChoosingAppArrow SVG" />
+          </div>
+          </button>
+      </div>}
+
+      {memberGroups.filter((item)=> item == "Sizing app").length == 1 && <div className="bg-white rounded-lg shadow dark:border md:m-10">
+      <button onClick={()=> navigate('/sizing-app-dashBoard')}>
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-4">
+          <img
+              src={SizingAppImg}
+              alt="SizingAppImg SVG" />
+          </div>
+
+          <div>
+          <h1 className="text-xs font-bold leading-tight tracking-tight text-gray-900 md:text-xs dark:text-white">Sizing App</h1>
+          <p className="text-sm m-6">Lorem ipsum dolor sit amet, <br/>consectetur adipiscing</p>
+          </div>
+
+          <div className="bg-sky">
+          <img
+          className="bg-amber-300 ml-auto p-1"
+              src={ChoosingAppArrow}
+              alt="ChoosingAppArrow SVG" />
+          </div>
+           </button>
+      </div>}
+      <div>
       </div>
-    </div>
+  </div>
+      </div>
   );
 }
 export default MenuChooser;
