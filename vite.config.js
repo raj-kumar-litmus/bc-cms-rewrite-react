8b41154b055bf652c2af8af202a1954a8e996031
Raj Kumar <coding.godzilla@gmail.com>
2023-05-16 21:37:57 +0530

    Tweaking vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from "vite";
 import EnvironmentPlugin from "vite-plugin-environment"
 import react from '@vitejs/plugin-react'
-import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), EnvironmentPlugin("all")],
-  server : {
-    port: 3000,
-  }
-})
+// export default defineConfig({
+//   plugins: [react(), EnvironmentPlugin("all")],
+//   server : {
+//     port: 3000,
+//   }
+// })
+
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return defineConfig({
+    define: {
+      "process.env": env,
+    },
+    plugins: [react()],
+    server : {
+      port: 3000,
+    }
+  });
+};
