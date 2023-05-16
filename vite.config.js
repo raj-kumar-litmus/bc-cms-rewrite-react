a2606c3e9bd9df0f4fce44c3e9ed0f7b114802e3
Raj Kumar <coding.godzilla@gmail.com>
2023-05-16 21:39:40 +0530

    Remove commented lines from vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
-import EnvironmentPlugin from "vite-plugin-environment"
 import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react(), EnvironmentPlugin("all")],
-//   server : {
-//     port: 3000,
-//   }
-// })
 
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
