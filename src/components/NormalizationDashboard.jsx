794092a214907e253950c5eded1faf8e39dd1ef3
Raj Kumar <coding.godzilla@gmail.com>
2023-05-16 21:27:41 +0530

    Fixing indentation

--- src/components/NormalizationDashboard.jsx
+++ src/components/NormalizationDashboard.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import StatusBarsForNormalization from './StatusBarsForNormalization.jsx';
-import GlobalSearch from './GlobalSearch.jsx';
-import WriterDashBoardTabs from './WriterDashBoardTabs.jsx';
+import React from "react";
+import StatusBarsForNormalization from "./StatusBarsForNormalization.jsx";
+import GlobalSearch from "./GlobalSearch.jsx";
+import WriterDashBoardTabs from "./WriterDashBoardTabs.jsx";
 
 function NormalizationDashboard() {
   return (
-    <div className='bg-bg'>
-       <div className='m-2'>
-        <StatusBarsForNormalization/>
-       </div>
-       <div className='m-10'>
-        <GlobalSearch
-        searchString={"Search"}/>
-       </div>
-       <div className='m-10'>
-        <WriterDashBoardTabs/>
-       </div>
+    <div className="bg-bg">
+      <div className="m-2">
+        <StatusBarsForNormalization />
+      </div>
+      <div className="m-10">
+        <GlobalSearch searchString={"Search"} />
+      </div>
+      <div className="m-10">
+        <WriterDashBoardTabs />
+      </div>
     </div>
   );
 }
--- src/components/WriterDashBoardTabs.jsx
+++ src/components/WriterDashBoardTabs.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 import { useState, useEffect } from "react";
 import useSessionStorage from "../hooks/useSessionStorage";
-import Button from './Button';
+import Button from "./Button";
 
 function WriterDashBoardTabs() {
   const [isAdmin, setIsAdmin] = useState(false);
@@ -13,41 +13,68 @@ function WriterDashBoardTabs() {
     setIsAdmin(groups?.includes(ADMIN_GROUP_ID));
   }, [ADMIN_GROUP_ID, setIsAdmin]);
 
-    const handleTabEvents = (tab) => {
-      setCurrentTab(tab.target.innerText);
-    };
+  const handleTabEvents = (tab) => {
+    setCurrentTab(tab.target.innerText);
+  };
 
   return (
     <div className="text-sm border-b dark:border-bg">
-    <div className="flex">
-      {isAdmin && (
-        <Button
-        onClick={handleTabEvents}
-        currentTab={currentTab}
-        className={currentTab == "Unassigned" ? "py-2 px-8 font-bold border-b-2" : "py-2 px-8"}
-        > Unassigned </Button>
-      )}
+      <div className="flex">
+        {isAdmin && (
+          <Button
+            onClick={handleTabEvents}
+            currentTab={currentTab}
+            className={
+              currentTab == "Unassigned"
+                ? "py-2 px-8 font-bold border-b-2"
+                : "py-2 px-8"
+            }
+          >
+            {" "}
+            Unassigned{" "}
+          </Button>
+        )}
 
-      <Button
-        onClick={handleTabEvents}
-        currentTab={currentTab}
-        className={currentTab == "Completed" ? "py-2 px-8 font-bold border-b-2" : "py-2 px-8"}
-        > Completed
+        <Button
+          onClick={handleTabEvents}
+          currentTab={currentTab}
+          className={
+            currentTab == "Completed"
+              ? "py-2 px-8 font-bold border-b-2"
+              : "py-2 px-8"
+          }
+        >
+          {" "}
+          Completed
         </Button>
 
-       <Button
-        onClick={handleTabEvents}
-        currentTab={currentTab}
-        className={currentTab == "Assigned" ? "py-2 px-8 font-bold border-b-2" : "py-2 px-8"}
-        > Assigned </Button>
+        <Button
+          onClick={handleTabEvents}
+          currentTab={currentTab}
+          className={
+            currentTab == "Assigned"
+              ? "py-2 px-8 font-bold border-b-2"
+              : "py-2 px-8"
+          }
+        >
+          {" "}
+          Assigned{" "}
+        </Button>
 
-      <Button
-        onClick={handleTabEvents}
-        currentTab={currentTab}
-        className={currentTab == "InProgress" ? "py-2 px-8 font-bold border-b-2" : "py-2 px-8"}
-        > InProgress </Button>
+        <Button
+          onClick={handleTabEvents}
+          currentTab={currentTab}
+          className={
+            currentTab == "InProgress"
+              ? "py-2 px-8 font-bold border-b-2"
+              : "py-2 px-8"
+          }
+        >
+          {" "}
+          InProgress{" "}
+        </Button>
+      </div>
     </div>
-  </div>
   );
 }
 export default WriterDashBoardTabs;
