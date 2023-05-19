42a282a16338cd12034d8a887469b72ba10fda30
deepthi tirupati <deepthi.tirupathi@backcountry.com>
2023-05-19 11:30:21 +0530

    review comments

--- src/components/GlobalSearch.jsx
+++ src/components/GlobalSearch.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Button from './Button';
 
-function GlobalSearch({searchString, className}) {
+function GlobalSearch({searchString, inputClasses, buttonClasses}) {
     return (
           <form className="flex">  
-            <input type="search" className={className} placeholder="Search by Style or Title or Brand"/>
-            <Button className={"text-white bg-black h-[64px] w-[131px] rounded ml-2"}> {searchString} </Button>
+            <input type="search" className={inputClasses} placeholder="Search by Style or Title or Brand"/>
+            <Button className={buttonClasses}> {searchString} </Button>
          </form>
     );
   }
