d50c768a296262374ff39f2f8559990cbae36fd6
deepthi tirupati <deepthi.tirupathi@backcountry.com>
2023-05-19 11:34:20 +0530

    review comments

--- src/components/NormalizationDashboard.jsx
+++ src/components/NormalizationDashboard.jsx
@@ -9,7 +9,7 @@ function NormalizationDashboard() {
       <div className="mx-[139px]">
         <StatusBarsForNormalization />
       <div className="mt-[40px]">
-        <GlobalSearch searchString={"Search"} inputClasses="bg-white w-full h-[64px] items-center pl-[24px] text-sm placeholder-gray-20 rounded border border-grey-30 shadow"
+        <GlobalSearch searchString={"Search"} inputClasses={"bg-white w-full h-[64px] items-center pl-[24px] text-sm placeholder-gray-20 rounded border border-grey-30 shadow"}
         buttonClasses={"text-white bg-black h-[64px] w-[131px] rounded ml-2"} />
       </div>
       <div className="mt-[49px]">
