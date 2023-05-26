982b69dd1d2979718cd9527d26ea8016949744f6
Raj Kumar <coding.godzilla@gmail.com>
2023-05-26 11:55:05 +0530

    Review comments : Bug fixes

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,13 @@
 const { VITE_SERVER_HOST_NAME: HOST_NAME } = process.env;
 
 export const workFlowsUrl = `${HOST_NAME}/api/v1/workflows`;
+
+export const status = {
+  waitingForWriter: "WAITING_FOR_WRITER",
+  assignedToWriter: "ASSIGNED_TO_WRITER",
+  writingInProgress: "WRITING_IN_PROGRESS",
+  writingComplete: "WRITING_COMPLETE",
+  assignedToEditor: "ASSIGNED_TO_EDITOR",
+  editingInProgress: "EDITING_IN_PROGRESS",
+  editingComplete: "EDITING_COMPLETE"
+};
