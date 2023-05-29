f90c22ca08850da8160f24938c115a758089b15c
deepthi tirupati <deepthi.tirupathi@backcountry.com>
2023-05-29 17:36:02 +0530

    search and brand API changes

--- src/components/GlobalSearch.jsx
+++ src/components/GlobalSearch.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Button from './Button';
 
-function GlobalSearch({searchString, inputClasses, buttonClasses}) {
+function GlobalSearch({searchString, inputClasses, buttonClasses, onChange, onClick, value}) {
     return (
           <div className="flex">  
-            <input className={inputClasses} placeholder="Search by Style or Title or Brand"/>
-            <Button className={buttonClasses}> {searchString} </Button>
+            <input className={inputClasses} value={value} placeholder="Search by Style or Title or Brand" onChange={onChange}/>
+            <Button className={buttonClasses} onClick={onClick}> {searchString} </Button>
          </div>
     );
   }
