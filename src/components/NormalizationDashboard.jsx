7c40cc1bd8f319f0cb519ea222172df6ff526ec4
deepthi tirupati <deepthi.tirupathi@backcountry.com>
2023-05-29 12:14:10 +0530

    sort icon changes

--- src/components/NormalizationDashboard.jsx
+++ src/components/NormalizationDashboard.jsx
@@ -40,6 +40,22 @@ function NormalizationDashboard() {
     setSelectedProducts([]);
     clearFilters()
   };
+  const handleSearchChange=(e)=>{
+    setSearch(e.target.value)
+    console.log("e>>",e.target.value)
+  }
+
+  const handleSearchClick=()=>{
+    console.log("Search",search)
+    fetch(`${workFlowsUrl}/search?limit=10&page=${currentPage}&global`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+      .then((response) => response.json())
+      .then((result) => setStatus(result?.data?.uniqueValues));
+  }
 
   const fetchBulkStyleSearch = async () => {
     try {
