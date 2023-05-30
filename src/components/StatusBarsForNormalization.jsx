34b77357bf650bdd9853bd75bd6a345387dcca15
deepthi tirupati <deepthi.tirupathi@backcountry.com>
2023-05-30 11:40:38 +0530

    tost alignment

--- src/components/StatusBarsForNormalization.jsx
+++ src/components/StatusBarsForNormalization.jsx
@@ -39,15 +39,11 @@ function StatusBarsForNormalization() {
     }
   },[showToast])
 
-
   return (
     <>
     <div
       className={isAdmin ? "grid grid-cols-4 gap-4" : "grid grid-cols-3 gap-4"}
     >
-      <div className="card flex justify-content-center">
-            <Toast ref={toast} />
-        </div>
       {isAdmin && (
         <StatusBar
           title={"Unassigned"}
