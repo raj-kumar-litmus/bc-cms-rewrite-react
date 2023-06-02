46c0a65b56fc56519a41532b476994d7054891ed
deepthi tirupati <deepthi.tirupathi@backcountry.com>
2023-06-02 11:34:08 +0530

    global search

--- src/components/GlobalSearch.jsx
+++ src/components/GlobalSearch.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import Button from './Button';
 
-function GlobalSearch({searchString, inputClasses, buttonClasses, onChange, onClick, value}) {
+function GlobalSearch({searchString, inputClasses, buttonClasses, onChange, onClick, value, handleClear}) {
     return (
-          <div className="flex">  
+          <div className="flex">
+             {value && <Button onClick={handleClear}>
+                <img
+                  className="h-[16px] w-[16px] relative top-[35px] left-[969px]"
+                  src={img}
+                  alt="NavBarSwitchingIcon SVG"
+                />
+          </Button>}
             <textarea value={value} className={inputClasses} rows="4" placeholder="Search by Style or Title or Brand" onChange={onChange}></textarea>
             <Button className={buttonClasses} onClick={onClick}> {searchString} </Button>
          </div>
