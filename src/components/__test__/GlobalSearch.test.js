201bba41ee885847ea7af431cc6eda79f4d698c8
deepthi tirupati <deepthi.tirupathi@backcountry.com>
2023-06-04 22:41:16 +0530

    search test case

--- src/components/__test__/GlobalSearch.test.js
+++ src/components/__test__/GlobalSearch.test.js
@@ -1,15 +1,14 @@
 import React from "react";
-import { render, screen, act } from "@testing-library/react";
+import { render, screen, act, fireEvent } from "@testing-library/react";
 import GlobalSearch from "../GlobalSearch";
 import { BrowserRouter } from "react-router-dom";
 
 describe("<GlobalSearch />", () => {
   it("render GlobalSearch page", async () => {
-    let queryByText;
     let rendered;
 
     await act(async () => {
-      rendered = render(<GlobalSearch value={true}  searchString={"search"}/>, {
+      rendered = render(<GlobalSearch value={true}  searchString={"Search"}/>, {
         wrapper: BrowserRouter
       });
     });
@@ -17,5 +16,10 @@ describe("<GlobalSearch />", () => {
     expect(
       getByPlaceholderText("Search by Style or Title or Brand")
     ).toBeTruthy();
+    expect(screen.queryByText("Search")).toBeTruthy();
+    const cancelButton = screen.queryByText("Search");
+    fireEvent.click(cancelButton);
+    const closeButton = screen.getByPlaceholderText("Search by Style or Title or Brand");
+    fireEvent.change(closeButton);
   });
 });
