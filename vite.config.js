d6e91595dc7a321b1d954f3bdeeb2e6cf4ef8a00
Raj Kumar <coding.godzilla@gmail.com>
2023-06-06 16:37:04 +0530

    [feat] Changing port in vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig, loadEnv } from "vite";
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
 
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return defineConfig({
     define: {
-      "process.env": env,
+      "process.env": env
     },
     plugins: [react()],
-    server : {
-      port: 3000,
+    server: {
+      port: 5173
     }
   });
 };
