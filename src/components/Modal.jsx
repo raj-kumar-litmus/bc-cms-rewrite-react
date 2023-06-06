3bb044f8c10517f807db9efff05bc76874625cd5
deepthi tirupati <deepthi.tirupathi@backcountry.com>
2023-06-06 08:58:04 +0530

    view history changes

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import { Dialog } from "primereact/dialog";
 
-function Modal({ visible, setVisible, className, children, header }) {
+function Modal({ visible, setVisible, className, children, header, footer }) {
   return (
     <Dialog
       header={header}
       visible={visible}
       className={className}
       aria-label="modal-pop-up"
+      footer={footer}
       onHide={() => setVisible(false)}
     >
       {children}
--- src/pages/ViewHistoryModel.jsx
+++ src/pages/ViewHistoryModel.jsx
@@ -1,40 +1,75 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Modal from "../components/Modal";
-import Loader from "../components/loader";
-import { Timeline } from 'primereact/timeline';
+import Button from "../components/Button";
 
-function ViewHistoryModel({showHistoryPopup, setShowHistoryPopup}) {
-    const events = [
-        { status: 'Ordered', date: '15/10/2020 10:30', icon: 'pi pi-shopping-cart', color: '#9C27B0', image: 'game-controller.jpg' },
-        { status: 'Processing', date: '15/10/2020 14:00', icon: 'pi pi-cog', color: '#673AB7' },
-        { status: 'Shipped', date: '15/10/2020 16:15', icon: 'pi pi-shopping-cart', color: '#FF9800' },
-        { status: 'Delivered', date: '16/10/2020 10:00', icon: 'pi pi-check', color: '#607D8B' }
-    ];
+function ViewHistoryModel({ showHistoryPopup, setShowHistoryPopup }) {
+  const events = [
+    {
+      status: "15 hours ago David John has made some changes",
+    },
+    {
+      status: "1 day ago Tom Sears has made some changes",
+    },
+    {
+      status: "2 day ago Lionel Taurus has made some changes",
+    },
+    {
+      status: "3 day ago Steven Neff has made some changes",
+    },
+    {
+      status: "5 day ago David John has made some changes",
+    }
+  ];
+
+  const footerContent = () => {
+    return (
+      <div className="flex justify-center">
+        <Button
+          onClick={handleViewMoreHistory}
+          className={
+            "bg-gray-900 border text-white rounded border-gray-800 !w-[165px] h-[39px]"
+          }
+        >
+          View more history
+        </Button>
+      </div>
+    );
+  };
+
+  const hanldeViewDetails=()=>{
+
+  }
+
+  const handleViewMoreHistory=()=>{
+
+  }
+
   return (
     <>
       <Modal
-        className={"w-[500px] rounded-full"}
+        className={"w-[600px] rounded-full"}
         visible={showHistoryPopup}
         setVisible={setShowHistoryPopup}
-        header={<p className="text-[18px] text-[#2C2C2C] text-center ">{"History"}</p>}
+        header={
+          <p className="text-[18px] text-[#2C2C2C] text-center ">{"History"}</p>
+        }
+        footer={footerContent}
       >
-    <ol class="relative border-l border-[#2C2C2C]">                  
-      <li class="mb-10 ml-4">
-        <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.6 -left-1.5 border border-[#2C2C2C] bg-white"></div>
-        <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">February 2022</time>
-        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Application UI code in Tailwind CSS</h3>
-        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
-        <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Learn more <svg class="w-3 h-3 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></a>
-       </li>
-       
-       <li class="mb-10 ml-4">
-        <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.6 -left-1.5 border border-[#2C2C2C] bg-white"></div>
-        <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">February 2022</time>
-        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Application UI code in Tailwind CSS</h3>
-        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
-        <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Learn more <svg class="w-3 h-3 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></a>
-       </li>
-    </ol>
+        <ol class="relative border-l border-[#2C2C2C] mt-[5%]">
+          {events.map((item) => (
+            <li class="mb-5 ml-4">
+              <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.6 -left-1.5 border border-[#2C2C2C] bg-white"></div>
+              <div className="flex justify-between">
+                <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400 w-[330px]">
+                  {item.status}
+                </p>
+                <Button className="border border-[#1C1C1C] text-[#1C1C1C] rounded h-[30px] w-[106px] text-[13px] justify-end" onClick={hanldeViewDetails}>
+                  View Details
+                </Button>{" "}
+              </div>
+            </li>
+          ))}
+        </ol>
       </Modal>
     </>
   );
